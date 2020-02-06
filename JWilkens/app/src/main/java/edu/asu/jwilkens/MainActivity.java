b943d09b303745a59c5f406e4cb84e6b7df35db3
Jacob Wilkens (jwilken1) <jwilken1@asu.edu>
2020-02-05 21:30:48 -0500

    Updatez

--- JWilkens/app/src/main/java/edu/asu/jwilkens/MainActivity.java
+++ JWilkens/app/src/main/java/edu/asu/jwilkens/MainActivity.java
@@ -8,6 +8,8 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     // Graph related values
     private GraphView health_graph;
@@ -16,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     //private static String[] graph_horlabels = {"2700", "2750", "2800", "2850", "2900",
             //"2950", "3000", "3050", "3100"};
     //private static String[] graph_verlabels = {"500", "1000", "1500", "2000"};
-    private static String[] graph_horlabels = {"0", "1", "2", "3", "4", "5", "6", "7"};
-    private static String[] graph_verlabels = {"0", "1", "2", "3"};
+    private static String[] graph_horlabels = {"0", "1", "2", "3", "4"};
+    private static String[] graph_verlabels = {"4", "3", "2", "1", "0"};
     private static String graph_title = "Health Graph UI";
 
     // UI Related Elements
@@ -35,9 +37,10 @@ public class MainActivity extends AppCompatActivity {
     //Context context, float[] values, String title, String[] horlabels, String[] verlabels, boolean type
     private void createGraphView() {
         /* Data */
-        graph_values = new float[5];
-        for (int i = 0; i < 5; i++) {
-            graph_values[i] = i+550;
+        graph_values = new float[20];
+        Random rand = new Random();
+        for (int i = 0; i < 20; i++) {
+            graph_values[i] = rand.nextInt(21);
         }
         health_graph = new GraphView(this, graph_values, graph_title, graph_horlabels, graph_verlabels, GraphView.LINE);
         health_graph.setLayoutParams(new ConstraintLayout.LayoutParams(
